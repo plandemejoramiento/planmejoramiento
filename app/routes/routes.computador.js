293187c396555176d6da46f538084c5115f21ade
delanyroman04 <delayroman714@gmail.com>
2024-06-13 10:18:43 -0500

    thunder client

---
+++ app/routes/routes.computador.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { crearPc, eliminarPc, listarPc, modificarPc, mostrarPc } from "../controllers/controller.computador";
+
+const rutaComputador = Router();
+
+rutaComputador.get("/pc", mostrarPc);
+rutaComputador.get("/pc/:id", listarPc);
+rutaComputador.post("/pc", crearPc);
+rutaComputador.put("/pc/:id", modificarPc);
+rutaComputador.delete("/pc/:id", eliminarPc);
+
+export default rutaComputador;
